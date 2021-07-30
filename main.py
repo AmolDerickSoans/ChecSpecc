7bbc26073e1bb11626de1c86eda33396db2ace5a
Amol derick soans <amol.optimus@gmail.com>
2021-07-30 20:56:14 +0530

    added main.py as main file in module that communicates with other two

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+import sys
 
-from specchecc import *
-from makeFile import  *
+from specchecc import compare , connection_handler
+from makeFile import  getSystemInfo
+
+
+
+available_commands = {
+    'init':{
+        'name':'init',
+        'help':"Welcome to specchecc , with the init command you can make a recc.json file holding all \n the required specifications"
+    }
+}
+
+def main(argv):
+    if not len(argv) or argv[0] not in available_commands.keys():
+        print('Available commands: ')
+        for key, val in available_commands.items():
+            print('-> {}: {}'.format(
+                key, val['help']
+            ))
+        print('\n')
+        return
+    
+    task_name = argv[0]
+
+    if task_name == available_commands['init']['name']:
+        getSystemInfo()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+    connection_handler("cpu_speedtraq.db")
+    compare()
